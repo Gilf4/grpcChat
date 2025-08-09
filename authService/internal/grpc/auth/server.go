b9da9e9372474be03323485ea4825a13e1fb0ead
Gilf4 <esmirnov1234@gmail.com>
2025-08-09 13:16:35 +0300

    feat(auth/grpcHandlers):implemented auth handlers(Register, Login)

--- authService/internal/grpc/auth/server.go
+++ authService/internal/grpc/auth/server.go
@@ -3,22 +3,69 @@ package auth
 import (
 	"context"
 
-	"github.com/Gilf4/grpcChat/protos/gen/go/auth/v1"
+	authv1 "github.com/Gilf4/grpcChat/protos/gen/go/auth/v1"
 	"google.golang.org/grpc"
+	"google.golang.org/grpc/codes"
+	"google.golang.org/grpc/status"
 )
 
+type Auth interface {
+	Login(ctx context.Context, email string, password string) (token string, err error)
+	Register(ctx context.Context, email string, password string, name string) (userId int64, err error)
+}
+
 type serverAPI struct {
-	auth.UnimplementedAuthServer
+	authv1.UnimplementedAuthServer
+	auth Auth
 }
 
 func Register(gRPCServer *grpc.Server) {
-	auth.RegisterAuthServer(gRPCServer, &serverAPI{})
+	authv1.RegisterAuthServer(gRPCServer, &serverAPI{})
 }
 
-func (s *serverAPI) Login(ctx context.Context, req *auth.LoginRequest) (*auth.LoginResponse, error) {
-	panic("implement me")
+func (s *serverAPI) Login(ctx context.Context, req *authv1.LoginRequest) (*authv1.LoginResponse, error) {
+	email := req.GetEmail()
+	if email == "" {
+		return nil, status.Error(codes.InvalidArgument, "email is required")
+	}
+
+	password := req.GetPassword()
+	if password == "" {
+		return nil, status.Error(codes.InvalidArgument, "password is required")
+	}
+
+	token, err := s.auth.Login(ctx, email, password)
+	if err != nil {
+		//TODO ...
+
+		return nil, status.Error(codes.Internal, "failed to login")
+	}
+
+	return &authv1.LoginResponse{Token: token}, nil
 }
 
-func (s *serverAPI) Register(ctx context.Context, req *auth.RegisterRequest) (*auth.RegisterResponse, error) {
-	panic("implement me")
+func (s *serverAPI) Register(ctx context.Context, req *authv1.RegisterRequest) (*authv1.RegisterResponse, error) {
+	email := req.GetEmail()
+	if email == "" {
+		return nil, status.Error(codes.InvalidArgument, "email is required")
+	}
+
+	password := req.GetPassword()
+	if password == "" {
+		return nil, status.Error(codes.InvalidArgument, "password is required")
+	}
+
+	name := req.GetName()
+	if name == "" {
+		return nil, status.Error(codes.InvalidArgument, "name is required")
+	}
+
+	userId, err := s.auth.Register(ctx, email, password, name)
+	if err != nil {
+		//TODO ...
+
+		return nil, status.Error(codes.Internal, "failed to register")
+	}
+
+	return &authv1.RegisterResponse{UserId: userId}, nil
 }
